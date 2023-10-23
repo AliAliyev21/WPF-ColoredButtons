676b8a17f5754ec22743a1e803dd0a1131cad863
GitHub Kullanıcı Adınız <GitHub E-Posta Adresini>
2023-10-24 02:25:45 +0400

    My First App

---
+++ WpfFirstApplication/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFirstApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+
+            byte red = (byte)random.Next(256);
+            byte green = (byte)random.Next(256);
+            byte blue = (byte)random.Next(256);
+
+            SolidColorBrush randomBrush = new SolidColorBrush(Color.FromRgb(red, green, blue));
+            if(sender is Button btn)
+            {
+                btn.Background = randomBrush;
+                MessageBox.Show($"Hey I am Button {btn.Content}");
+            }
+        }
+
+        private void btn1_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(sender is Button btn)
+            {
+                if (btn.Parent is StackPanel parentStackPanel)
+                {
+                    parentStackPanel.Children.Remove(btn);
+                }
+                this.Title = "Button " + btn.Content + " Silindi";
+            }
+        }
+    }
+}
